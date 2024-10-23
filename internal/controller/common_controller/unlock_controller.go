eeab5247e9d30723618a4787574c091b822ce177
czqu <117530179+czqu@users.noreply.github.com>
2024-10-23 17:51:13 +0800

    style: fix confusing code

--- internal/controller/common_controller/unlock_controller.go
+++ internal/controller/common_controller/unlock_controller.go
@@ -42,7 +42,7 @@ func (o *UnlockController) Unlock(c *gin.Context) {
 
 	logger.Info("Username and password information have been received")
 	e := o.u.UnlockPc(reqData.UserName, reqData.Password)
-	if e == nil || !e.Equal(exception.ErrSuccess) {
+	if exception.ErrSuccess.NotEqual(e) {
 		logger.Info(e.Msg)
 		c.Error(e)
 		return
