3fd43cbf7d120e817650fcb4b950ea65d813baa7
czqu <117530179+czqu@users.noreply.github.com>
2024-12-18 20:38:28 +0800

    feat: add sentry

--- cmd/core-service/main.go
+++ cmd/core-service/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fadacontrol/internal/base/application"
 	"fadacontrol/internal/base/logger"
+	"fadacontrol/internal/base/version"
+	"fadacontrol/pkg/utils"
+	"github.com/getsentry/sentry-go"
+	"time"
 )
 
 func main() {
@@ -10,7 +15,23 @@ func main() {
 		if err := recover(); err != nil {
 			logger.Error(err)
 			logger.Sync()
+			e, ok := err.(error)
+			if !ok {
+				e = errors.New("panic occurred: " + utils.ConvertToString(err) + "  " + version.GetBuildInfo())
+			}
+			sentry.CaptureException(e)
 		}
 	}()
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:   "https://82431285059e21675920c08d0e172643@o4508488989605888.ingest.us.sentry.io/4508489034825728",
+		Debug: true,
+	})
+	sentry.ConfigureScope(func(scope *sentry.Scope) {
+
+		scope.SetTag("app_info", version.GetBuildInfo())
+
+	})
+	logger.Error(err)
+	defer sentry.Flush(5 * time.Second)
 	application.Execute()
 }
--- pkg/goroutine/recover.go
+++ pkg/goroutine/recover.go
@@ -1,8 +1,12 @@
 package goroutine
 
 import (
+	"errors"
 	"fadacontrol/internal/base/logger"
+	"fadacontrol/internal/base/version"
+	"fadacontrol/pkg/utils"
 	"fmt"
+	"github.com/getsentry/sentry-go"
 )
 
 func RecoverGO(f func()) {
@@ -10,6 +14,12 @@ func RecoverGO(f func()) {
 		defer func() {
 			if r := recover(); r != nil {
 				logger.Errorf(fmt.Sprintf("recover from panic: %v", r))
+				e, ok := r.(error)
+				if !ok {
+					e = errors.New("panic occurred: " + utils.ConvertToString(r) + "  " + version.GetBuildInfo())
+				}
+				sentry.CaptureException(e)
+				logger.Sync()
 			}
 		}()
 		f()
