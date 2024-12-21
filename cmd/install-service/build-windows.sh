a634d35eb781091637dde9263d4b31dd98f326a8
czqu <117530179+czqu@users.noreply.github.com>
2024-12-21 18:57:38 +0800

    fix: fix build

--- cmd/install-service/build-windows.sh
+++ cmd/install-service/build-windows.sh
@@ -2,4 +2,4 @@
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -buildmode=pie -trimpath -ldflags "-s -w -extldflags '-static -flto -O2 -Wl,--gc-sections'" -o ./out/install-service-x64.exe
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -buildmode=pie -trimpath -ldflags "-s -w  -extldflags '-static -flto -O2 -Wl,--gc-sections'" -o ./out/install-service-x86.exe
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -buildmode=pie -trimpath -ldflags "-s -w  -extldflags '-static -flto -O2 -Wl,--gc-sections'" -o ./out/install-service-arm64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -buildmode=pie -trimpath -ldflags "-s -w  -extldflags '-static -flto -O2 -Wl,--gc-sections'" -o ./out/install-service-arm64.exe
